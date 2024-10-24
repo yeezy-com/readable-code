2d231b4425e8a0f8a6fc0a74ad581e044e0005ca
GiSeok Kang <ime.rokz@gmail.com>
2024-10-24 20:10:24 +0900

    refactor: [추상>OCP] 게임의 난이도를 인터페이스로 하여 확장에 열리도록 함
    - 확장에는 열려 있고, 수정에는 닫혀 있어야 한다.
      - 기존 코드 변경 없이, 시스템 기능을 확장할 수 있어야 한다.
    - 추상화와 다형성을 활용해서 OCP를 지킬 수 있다.

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,9 +1,17 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.gamelevel.Advanced;
+import cleancode.minesweeper.tobe.gamelevel.Beginner;
+import cleancode.minesweeper.tobe.gamelevel.GameLevel;
+import cleancode.minesweeper.tobe.gamelevel.Middle;
+import cleancode.minesweeper.tobe.gamelevel.VeryBeginner;
+
 public class GameApplication {
 
     public static void main(String[] args) {
-        Minesweeper minesweeper = new Minesweeper();
+        GameLevel gameLevel = new VeryBeginner();
+
+        Minesweeper minesweeper = new Minesweeper(gameLevel);
         minesweeper.run();
     }
 }
